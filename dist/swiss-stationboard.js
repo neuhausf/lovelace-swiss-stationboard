f79360cfd692c9e063823f277bd35dc0ea385f12
Rudd-O <rudd-o@rudd-o.com>
2023-01-05 17:50:36 +0000

    Relabel and fix issues.

--- dist/swiss-stationboard.js
+++ dist/swiss-stationboard.js
@@ -147,14 +147,17 @@ class SwissPublicTransportCard extends LitElement {
       {
         const minutes = Math.floor(absoluttotalseconds / 60);
         const seconds = absoluttotalseconds % 60;
-        eta = "in ";
-        var minsStr = ((minutes > 1) ? " mins" : " min";
-        minsStr = this._config.minutes_string ? this._config.minutes_string : minsStr;
+        eta = "in";
+        var minsStr = (minutes > 1) ? " mins" : " min";
+        minsStr = this._config.minutes_label ? this._config.minutes_label : minsStr;
         var secsStr = "″";
-        var secsStr = this._config.seconds_string ? this._config.seconds_string : minsStr;
-        eta += minutes + minsStr);
+        var secsStr = this._config.seconds_label ? this._config.seconds_label : minsStr;
+        eta += " " + minutes + minsStr;
         if (this._config.show_seconds)
-          eta += seconds + secsStr;
+          eta += " " + seconds + secsStr;
+        // Corner case 0m 0s
+        if (eta == "in")
+          eta = "";
       }
       
       // allow category filtering by regex (S-Bahn, Bus, ...)
